d234bc5accb10774a6cfa4387018f3de79a060eb
sylar0012 <kmc42692@naver.com>
2022-10-21 14:09:24 +0900

    로그 나오는거 변경

--- src/main/java/site/metacoding/white/domain/Board.java
+++ src/main/java/site/metacoding/white/domain/Board.java
@@ -3,6 +3,7 @@ package site.metacoding.white.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -13,9 +14,10 @@ import lombok.Setter;
 @Entity
 public class Board {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // identity : 해당 DB의 id증가 전략을 따라감.
   private Long id;
   private String title;
   @Column(length = 1000)
   private String content;
+  private String author;
 }
